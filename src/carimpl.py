df8f4f587ed61b62df360c04ea58eef7b890039e
Julia Dlutek <jdlutek987@gmail.com>
2021-12-06 11:46:35 +0100

    Zadanie 2

---
+++ src/carimpl.py
@@ -0,0 +1,22 @@
+from src.car import Car
+
+
+class CarImpl:
+    def __init__(self, car: Car):
+        self.car = car
+
+    def carNeedsFuel(self):
+        if self.car.needsFuel():
+            return "Car needs fuel!"
+        return "Car doesn't need fuel."
+
+    def carGetEngineTemperature(self):
+        if self.car.getEngineTemperature():
+            temp = self.car.getEngineTemperature()
+            return "Car temperature is equal to " + str(temp) + "`C"
+        return "Car temperature is unknown..."
+
+    def carDriveTo(self, destination):
+        if self.car.driveTo(destination):
+            return "Car is driving to " + str(destination)
+        return "Car isn't driving now."
---
+++ tests/test_car.py
@@ -0,0 +1,48 @@
+import unittest
+from unittest.mock import *
+from src.car import Car
+from src.carimpl import CarImpl
+
+
+class TestCar(unittest.TestCase):
+    def test_needs_fuel_true(self):
+        car = Car()
+        car.needsFuel = Mock(name='needsFuel')
+        car.needsFuel.return_value = True
+        carImpl = CarImpl(car)
+        self.assertEqual(carImpl.carNeedsFuel(), "Car needs fuel!")
+
+    def test_needs_fuel_false(self):
+        car = Car()
+        car.needsFuel = Mock(name='needsFuel')
+        car.needsFuel.return_value = False
+        carImpl = CarImpl(car)
+        self.assertEqual(carImpl.carNeedsFuel(), "Car doesn't need fuel.")
+
+    def test_temperature_100(self):
+        car = Car()
+        car.getEngineTemperature = Mock(name='getEngineTemperature')
+        car.getEngineTemperature.return_value = 100
+        carImpl = CarImpl(car)
+        self.assertEqual(carImpl.carGetEngineTemperature(), "Car temperature is equal to 100`C")
+
+    def test_temperature_undefined(self):
+        car = Car()
+        car.getEngineTemperature = Mock(name='getEngineTemperature')
+        car.getEngineTemperature.return_value = None
+        carImpl = CarImpl(car)
+        self.assertEqual(carImpl.carGetEngineTemperature(), "Car temperature is unknown...")
+
+    def test_driving_to_Gdansk(self):
+        car = Car()
+        car.driveTo = Mock(name='driveTo')
+        car.driveTo.return_value = "Gdańsk"
+        carImpl = CarImpl(car)
+        self.assertEqual(carImpl.carDriveTo("Gdańsk"), "Car is driving to Gdańsk")
+
+    def test_not_driving(self):
+        car = Car()
+        car.driveTo = Mock(name='driveTo')
+        car.driveTo.return_value = None
+        carImpl = CarImpl(car)
+        self.assertEqual(carImpl.carDriveTo(None), "Car isn't driving now.")
